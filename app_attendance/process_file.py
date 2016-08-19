a413295ee8e2c160d840b853b69fce23f3d67903
tkovs <victor.rdg@hotmail.com>
2016-08-19 16:54:57 -0300

    Função sum_hours

--- app_attendance/process_file.py
+++ app_attendance/process_file.py
@@ -15,7 +15,26 @@ def get_input(inFile):
         item = {"id": splited_line[2], "name": splited_line[3].strip(), "date": date}
         input_list.append(item)
 
-    return input_list	
+    return sum_hours([person["date"] for person in input_list if person["id"] == "000000001"][1:8])
+    #return [person["date"] for person in input_list if person["id"] == "000000001"][1:8]
+    #return sum_hours([input_list[5], input_list[11]])
+
+def elapsed_time(start, end):
+    return (end - start)
+
+def sum_hours(hours):
+    """ Retorna a quantidade de horas 'trabalhadas' nessa lista de horários """
+
+    splited_hours = []
+    hours.reverse()
+
+    if (len(hours) % 2 != 0):
+        hours.pop()
+
+    while(hours):
+        splited_hours += [[hours.pop(), hours.pop()]]
+
+    return [elapsed_time(period[0], period[1]) for period in splited_hours]
 	
 def main():
     pass
