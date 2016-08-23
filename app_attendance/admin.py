c36467982ca19f7289057f1617d735cddc272342
tkovs <victor.rdg@hotmail.com>
2016-08-23 16:52:51 -0300

    Importa todos usuários para o banco de dados

--- app_attendance/admin.py
+++ app_attendance/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Person, Dedada
 
-# Register your models here.
+admin.site.register(Person)
+admin.site.register(Dedada)
--- app_attendance/process_file.py
+++ app_attendance/process_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from datetime import datetime, timedelta, date
+from .models import Person, Dedada
 
 def get_input(inFile):
     # Lê arquivo ignorando primeira e última linhas
@@ -15,10 +16,19 @@ def get_input(inFile):
         input_list.append(item)
 
     test_list = [{"id": person["id"], "date": person["date"], "name": person["name"]} for person in input_list]
-    return split_people(test_list)
+
+    for dedada in test_list:
+        if (not Person.objects.filter(id=dedada["id"])):
+            criar_usuario(dedada["id"], dedada["name"])
+
+    return test_list
+
+def criar_usuario(id, nome):
+    person = Person.objects.create(id=id, name=nome)
+    person.save()
 
 def main():
-    print (split_people())
+    pass
 
 if __name__ == '__main__':
 	main()
