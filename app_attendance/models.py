a3b2bbf3848c9e9e07d54a16cd201297128aeae9
tkovs <victor.rdg@hotmail.com>
2016-08-23 16:39:05 -0300

    Modelo de dados

--- app_attendance/models.py
+++ app_attendance/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    id   = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length=14)
+
+class Dedada(models.Model):
+    person = models.ForeignKey(Person, on_delete = models.CASCADE)
+    date   = models.DateTimeField(blank = False)
