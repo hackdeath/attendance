1067ba982af4144de4586fede361507e757609a9
AntonioVitor <antoniovitorFBN@gmail.com>
2016-08-24 16:42:05 -0300

    Correção dos links nos formulários dos templates

--- app_attendance/urls.py
+++ app_attendance/urls.py
@@ -3,6 +3,7 @@ from .                import views
 
 app_name = 'app_attendance'
 urlpatterns = [
-    url(r'^$',        views.index,   name='index'),
-    url(r'^display/', views.display, name='display'),
+    url(r'^$',        			views.index,   				name='index'			),
+    url(r'^display_per_month/', views.display_per_month, 	name='display_per_month'),
+    url(r'^display_per_day/', 	views.display_per_day, 		name='display_per_day'	),
 ]
