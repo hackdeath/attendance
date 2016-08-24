e13b4d1229557be7a9735a4838d13227df8a9761
AntonioVitor <antoniovitorFBN@gmail.com>
2016-08-24 16:45:35 -0300

    Modificação dos relacionamentos dos templates

--- app_attendance/forms.py
+++ app_attendance/forms.py
@@ -1,4 +1,14 @@
 from django import forms
+from datetime import datetime, date
+
+# Modificando o type no html do formulário
+class DateWidget(forms.DateInput):
+	input_type = "date"
+
+class SearchWorkedTimeForm(forms.Form):
+	init_date = forms.DateField(label="Início:", widget=DateWidget(), required=False)
+	final_date = forms.DateField(label="Final:", widget=DateWidget(), required=False)
+	name = forms.CharField(label="Nome", required=False)
 
 class UploadFileForm(forms.Form):
 	file = forms.FileField()
--- app_attendance/views.py
+++ app_attendance/views.py
@@ -1,14 +1,42 @@
 from django.http      import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .forms           import UploadFileForm
+from .forms           import UploadFileForm, SearchWorkedTimeForm
 from .process_file    import *
 import datetime
 
+# To do list:
+# Implementar o retorno ao index quando o formulário estiver inválido
+# Construir view de confirmação quando um arquivo log for enviado
+# Trocar o retorno de index()
 def index(request):
-	form = UploadFileForm()
+	#Checando se o método do formulário está correto
+	if request.method == "POST":
+		#Preenchendo variáveis com os 
+		file_form = UploadFileForm(request.POST, request.FILES, prefix="file")
+		search_form = SearchWorkedTimeForm(request.POST, prefix="search")
+		if file_form.is_valid():
+			ufile = request.FILES['file']
+			input = get_input(ufile.read().decode("UTF-8"))
+			return HttpResponse("Upload do arquivo log feito com sucesso.")
+
+		elif search_form.is_valid():
+			return HttpResponse("Busca pronta para ser efetuada")
+	
+	file_form = UploadFileForm(prefix="file")
+	search_form = SearchWorkedTimeForm(prefix="search")
+	form = {"file": file_form, "search": search_form}
+	
+	return render(request, 'app_attendance/index.html', {'form': form})		
+
+
+
+
+
+
+	form = {"file": file_form, "search": search_form}
 	return render(request, 'app_attendance/index.html', {'form': form})
 
-def display(request):
+def display_per_month(request):
 	if request.method == 'POST':
 		#Preenchendo o formulário com os dados
 		form = UploadFileForm(request.POST, request.FILES)
@@ -25,4 +53,5 @@ def display(request):
 	else:
 		return HttpResponse('Método de  formulário inválido')
 
-	return HttpResponse("Hello World")
+def display_per_day(request):
+	return HttpResponse("Ainda não foi feito")
