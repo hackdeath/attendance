a097db34b1b3059d3bbc1b0c03ef8fa4dbc83cf4
AntonioVitor <antoniovitorFBN@gmail.com>
2016-08-25 16:24:47 -0300

    Atualização das urls no urls.py e nos templates

--- app_attendance/urls.py
+++ app_attendance/urls.py
@@ -3,7 +3,5 @@ from .                import views
 
 app_name = 'app_attendance'
 urlpatterns = [
-    url(r'^$',        			views.index,   				name='index'			),
-    url(r'^display_per_month/', views.display_per_month, 	name='display_per_month'),
-    url(r'^display_per_day/', 	views.display_per_day, 		name='display_per_day'	),
+    url(r'^$', views.index, name='index'),
 ]
