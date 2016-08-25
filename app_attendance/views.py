6e8843e474a916a740e2145101ca85702e66e2b0
AntonioVitor <antoniovitorFBN@gmail.com>
2016-08-25 16:25:27 -0300

    Atualização do método view com dois forms Finalizado

--- app_attendance/views.py
+++ app_attendance/views.py
@@ -8,50 +8,40 @@ import datetime
 # Implementar o retorno ao index quando o formulário estiver inválido
 # Construir view de confirmação quando um arquivo log for enviado
 # Trocar o retorno de index()
+def display_per_month(search_form):
+	return HttpResponse("Busca de fingerprint por mês")
+
+def display_per_day(search_form):
+	return HttpResponse("Busca de fingerprint por dia")
+
 def index(request):
 	#Checando se o método do formulário está correto
 	if request.method == "POST":
-		#Preenchendo variáveis com os 
-		file_form = UploadFileForm(request.POST, request.FILES, prefix="file")
-		search_form = SearchWorkedTimeForm(request.POST, prefix="search")
-		if file_form.is_valid():
-			ufile = request.FILES['file']
-			input = get_input(ufile.read().decode("UTF-8"))
-			return HttpResponse("Upload do arquivo log feito com sucesso.")
+		#Para upload de arquivos
+		file_form = UploadFileForm(request.POST, request.FILES)
+		search_form = SearchWorkedTimeForm(request.POST)
+		if request.POST.get("file_upload__submit"):
+			#Checando se o formulário é válido
+			if file_form.is_valid():
+				#Fazendo upload do arquivo no banco de dados e retornando confirmação
+				ufile = request.FILES['file']
+				get_input(ufile.read().decode("UTF-8"))
+				return render(request, 'app_attendance/confirm_file_uploaded.html')
 
-		elif search_form.is_valid():
-			return HttpResponse("Busca pronta para ser efetuada")
+		#Para busca por mês
+		elif request.POST.get("search_per_month__submit"):
+			#Checando se o formulário é válido
+			if search_form.is_valid():
+				return display_per_month(search_form)
+		#Para busca por dia
+		elif request.POST.get("search_per_day__submit"):
+			#Checando se o formulário é válido
+			if search_form.is_valid():
+				return display_per_day(search_form)
+	#Caso não seja, criar formulário vazio
+	else:
+		file_form = UploadFileForm()
+		search_form = SearchWorkedTimeForm()
 	
-	file_form = UploadFileForm(prefix="file")
-	search_form = SearchWorkedTimeForm(prefix="search")
-	form = {"file": file_form, "search": search_form}
-	
-	return render(request, 'app_attendance/index.html', {'form': form})		
-
-
-
-
-
-
 	form = {"file": file_form, "search": search_form}
 	return render(request, 'app_attendance/index.html', {'form': form})
-
-def display_per_month(request):
-	if request.method == 'POST':
-		#Preenchendo o formulário com os dados
-		form = UploadFileForm(request.POST, request.FILES)
-
-		if form.is_valid():
-			ufile = request.FILES['file']
-			input = get_input(ufile.read().decode("UTF-8"))
-			initDate = datetime.datetime(2016,1,1)
-			finalDate = datetime.datetime.today()
-			input = filter_by_period(input, initDate, finalDate)
-			return render(request, 'app_attendance/display.html', {'months': input})
-		else:
-			return HttpResponse('Preenchimento do formulário inválido')
-	else:
-		return HttpResponse('Método de  formulário inválido')
-
-def display_per_day(request):
-	return HttpResponse("Ainda não foi feito")
