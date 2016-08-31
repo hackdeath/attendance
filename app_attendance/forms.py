71e2b67cc17fa734d18a6dc10519d4567bbc8a1c
tkovs <victor.rdg@hotmail.com>
2016-08-31 14:48:07 -0300

    CSS no index

--- app_attendance/forms.py
+++ app_attendance/forms.py
@@ -6,9 +6,9 @@ class DateWidget(forms.DateInput):
 	input_type = "date"
 
 class SearchWorkedTimeForm(forms.Form):
-	init_date = forms.DateField(label="Início:", widget=DateWidget(), required=False)
-	final_date = forms.DateField(label="Final:", widget=DateWidget(), required=False)
-	name = forms.CharField(label="Nome", required=False)
+	init_date  = forms.DateField(label="Início:", widget=DateWidget(), required=False)
+	final_date = forms.DateField(label="Final:",  widget=DateWidget(), required=False)
+	name       = forms.CharField(label="Nome", required=False)
 
 class UploadFileForm(forms.Form):
 	file = forms.FileField()
