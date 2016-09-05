1e5c90c0366bc134013cf983ee4bb5cf29364524
AntonioVitor <antoniovitorFBN@gmail.com>
2016-09-05 16:53:43 -0300

    display_per_day() finalizado com o novo model

--- app_attendance/models.py
+++ app_attendance/models.py
@@ -14,11 +14,17 @@ class Date(models.Model):
       return "{0}".format(self.date_fingerprint)
 
 class WorkedTime(models.Model):
-    person   = models.ForeignKey(Person, on_delete = models.CASCADE, null = True)
-    date     = models.ForeignKey(Date,   on_delete = models.CASCADE, null = True)
+    person   = models.ForeignKey(Person, on_delete = models.CASCADE, null = True, related_name="work_times", related_query_name="work_times")
+    date     = models.ForeignKey(Date,   on_delete = models.CASCADE, null = True, related_name="work_times", related_query_name="work_times")
     
-    initial  = models.TimeField(null = True)
+    initial  = models.TimeField(null = False)
     final    = models.TimeField(null = True)
 
     def __str__(self):
-      return "Person: {0} Date: {1} Initial: {2} Final: {3}".format(self.person, self.date, self.initial, self.final)
+        return "Person: {0} Date: {1} Initial: {2} Final: {3}".format(self.person, self.date, self.initial, self.final)
+
+    def calc_timedelta():
+        try:
+            return final - initial
+        except AttibuteError:
+            return timedelta(seconds=0)
