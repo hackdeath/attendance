2903d9ffbe3869b139670ec43c40a56ecce9aa02
AntonioVitor <antoniovitorFBN@gmail.com>
2016-09-08 16:49:55 -0300

    Valor inicial nas datas adicionado

--- app_attendance/forms.py
+++ app_attendance/forms.py
@@ -6,8 +6,8 @@ class DateWidget(forms.DateInput):
 	input_type = "date"
 
 class SearchWorkedTimeForm(forms.Form):
-	init_date  = forms.DateField(label="Início:", widget=DateWidget(), required=False)
-	final_date = forms.DateField(label="Final:",  widget=DateWidget(), required=False)
+	init_date  = forms.DateField(label="Início:", widget=DateWidget(), required=False, initial=date(date.today().year, 1, 1))
+	final_date = forms.DateField(label="Final:",  widget=DateWidget(), required=False, initial=date.today())
 	id       = forms.IntegerField(label="ID:", required=False)
 
 class UploadFileForm(forms.Form):
